1d80ac6be8c9144b78c2c4d19076d842c8389fd0
Nhoxboon <trannhatanh156@gmail.com>
2024-09-23 17:39:53 +0700

    Up

---
+++ src/bTap/Circle.java
@@ -0,0 +1,47 @@
+package bTap;
+
+public class Circle {
+
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+
+    public Circle(double r, String c) {
+        radius = r;
+        color = c;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double r) {
+        radius = r;
+    }
+
+    public void setColor(String c) {
+        color = c;
+    }
+
+    @Override
+    public String toString() {
+        return "bTap.Circle: radius=" + radius + " color=" + color;
+    }
+}
